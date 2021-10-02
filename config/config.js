693fff06ff3bd1e1d63aaf84564b49a9fa413c31
Hitsam Tiammar <hitsam.sidi@phincon.com>
2021-10-03 00:17:30 +0700

    add env

--- config/config.js
+++ config/config.js
@@ -1,9 +1,11 @@
+require('dotenv').config();
+
 module.exports = {
   development: {
-    username: 'postgres',
-    password: "123456789",
-    database: "fatbellies-xcidic",
-    host: "127.0.0.1",
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
     dialect: "postgres"
   },
   test: {
@@ -14,14 +16,14 @@ module.exports = {
     "dialect": "postgres"
   },
   "production": {
-    "username": "wqqqsscdfdppey",
-    "password": "ffacd8437e6fb7b65971124bfd59c842d17f888d9104468bfd1e554b11af8264",
-    "database": "d4h0qvtvtp2rje",
-    "host": "ec2-35-171-171-27.compute-1.amazonaws.com",
-    "dialect": "postgres",
-    "port": 5432,
-    "dialectOptions": {
-      "ssl": {
+    username: process.env.DB_USERNAME,
+    password:  process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    dialect: "postgres",
+    port: 5432,
+    dialectOptions: {
+      ssl: {
         require: true,
         rejectUnauthorized: false
       }
