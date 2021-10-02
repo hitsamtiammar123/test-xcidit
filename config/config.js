7bd8c2215cd6c05da633804fa740fbe1ccaf07ce
Hitsam Tiammar <hitsam.sidi@phincon.com>
2021-10-02 23:53:08 +0700

    add db configuration

--- config/config.js
+++ config/config.js
@@ -14,10 +14,10 @@ module.exports = {
     "dialect": "postgres"
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
+    "username": "wqqqsscdfdppey",
+    "password": "ffacd8437e6fb7b65971124bfd59c842d17f888d9104468bfd1e554b11af8264",
+    "database": "d4h0qvtvtp2rje",
+    "host": "ec2-35-171-171-27.compute-1.amazonaws.com",
     "dialect": "postgres"
   }
 };
