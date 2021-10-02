007902163adf25f6ae415a71f3d99c6015bca660
Hitsam Tiammar <hitsam.sidi@phincon.com>
2021-10-02 23:48:34 +0700

    add express config

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const cors=require('cors');
+const app = express();
+
+app.use(express.json())
+app.use(cors());
+
+app.get('/', (req,res) => {
+  res.send('Hello this is main page');
+})
+
+app.listen(process.env.PORT || 3020, () => {
+  console.log('Server listening')
+})
+
